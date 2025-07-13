83a332c6b13ba5562dc2f3d58f0a714e11901151
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-07-13 21:34:30 +0300

    Echo build with esbuild

---
+++ packages/echo/esbuild.config.ts
@@ -0,0 +1,51 @@
+import { BuildOptions } from 'esbuild';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+const config: BuildOptions = {
+  tsconfig: join(__dirname, './tsconfig.build.json'),
+
+  target: 'node22',
+  platform: 'node',
+
+  format: 'esm',
+
+  /**
+   * `@aws-sdk/*` is already bundled in AWS Lambda
+   */
+  external: ['@aws-sdk/*'],
+
+  bundle: true,
+  minify: true,
+
+  sourcemap: true,
+  sourcesContent: false,
+
+  logLevel: 'info',
+
+  loader: {
+    /**
+     * Excludes json from source map but keeps files separate
+     * which if fine since it all gets bundled into zip
+     */
+    '.json': 'copy',
+  },
+  plugins: [
+    {
+      name: 'excludeNodeModulesFromSourceMaps',
+      setup(build) {
+        // Don't generate source maps for vendor javascript files
+        build.onLoad({ filter: /node_modules.*(\.[cm]?[jt]sx?)$/ }, (args) => {
+          return {
+            contents:
+              readFileSync(args.path, 'utf8') +
+              '\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==',
+            loader: 'default',
+          };
+        });
+      },
+    },
+  ],
+};
+
+export default config;
---
+++ packages/echo/scripts/build.ts
@@ -0,0 +1,37 @@
+import { build, BuildOptions } from 'esbuild';
+import config from '../esbuild.config';
+
+await buildWithOptions(readArgs());
+
+function buildWithOptions(options: Pick<BuildOptions, 'entryPoints' | 'outfile'>) {
+  return build({
+    ...config,
+    entryPoints: options.entryPoints,
+    outfile: options.outfile,
+  });
+}
+
+function readArgs() {
+  const outFileIndex = process.argv.findIndex((value) => value === '--outfile');
+  if (outFileIndex === -1) {
+    throw new Error('Expected --outfile to be specified');
+  }
+  const outFile = process.argv[outFileIndex + 1];
+  if (!outFile) {
+    throw new Error('Expected --outfile to be specified');
+  }
+
+  const otherArgs = [
+    ...process.argv.slice(2, outFileIndex),
+    ...process.argv.slice(outFileIndex + 2),
+  ];
+
+  if (otherArgs.length === 0) {
+    throw new Error('Expected at least one entryPoint to be specified');
+  }
+
+  return {
+    outfile: outFile,
+    entryPoints: otherArgs,
+  };
+}
