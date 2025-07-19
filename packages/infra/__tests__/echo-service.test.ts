5fdae354f83c611aeffdc7d26a140ab5b4db6ed0
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2025-07-19 22:10:56 +0300

    infra follow conventions, scripts, ports

--- packages/infra/__tests__/echo-service.test.ts
+++ packages/infra/__tests__/echo-service.test.ts
@@ -1,7 +1,10 @@
 import { expect, it } from 'vitest';
 
+const PORT = process.env.API_PORT ?? '7000';
+const ENDPOINT = `http://localhost:${PORT}/echo`;
+
 it('echos back "Hello world!"', async () => {
-  const res = await fetch('http://localhost:3000/echo', {
+  const res = await fetch(ENDPOINT, {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain',
